a6f2e65fb9a477eabb551d631fc325f4d700b73d
Junhee Cho <junhee3085@gmail.com>
2020-11-19 11:29:28 +0900

    moving obstacle

--- functions.cpp
+++ functions.cpp
@@ -8,18 +8,18 @@ struct Player
     int x, y, life = 3; // current location and survival life
     static const int shape_size_y = 3, shape_size_x = 11;
     void appear(const int &move_y, const int &move_x){
-        mvaddstr(move_y-this->shape_size_y+1, move_x, " --------- ");    //Tayo bus
+        mvaddstr(move_y-this->shape_size_y+1, move_x," --------- ");    //Tayo bus
         mvaddstr(move_y-this->shape_size_y+2, move_x,"|      |OO|");
         mvaddstr(move_y-this->shape_size_y+3, move_x," --@--@--- ");
     }
 };
 
-struct Obj_RAPA_python
+struct obj_RAPA
 {
-    int x, y; // current location and survival life
+    int x=COLS - 1, y = LINES - 1; // current location and survival life
     static const int shape_size_y = 3, shape_size_x = 10;
     void appear(const int &move_y, const int &move_x){
-        mvaddstr(move_y-this->shape_size_y+1, move_x, " -------- ");    //Tayo bus
+        mvaddstr(move_y-this->shape_size_y+1, move_x," -------- ");    // enemy python
         mvaddstr(move_y-this->shape_size_y+2, move_x,"| Python |");
         mvaddstr(move_y-this->shape_size_y+3, move_x," -------- ");
     }
@@ -31,6 +31,7 @@ void display_map(char **, int, int);   // display map
 int is_move_ok(int, int);               // check obstacle to direction of moving
 struct Player move2direction(int ch, struct Player player);                    // move to direction
 struct Player gravityOfPlayer(int, struct Player);
+struct obj_RAPA moveObj(int, struct obj_RAPA);
 
 void game_start(){
     WINDOW *w;  // init curses
@@ -47,12 +48,21 @@ void game_start(){
     }
 	map_init(map, max_y, max_x);
 	Player player;  // define player
-    player.x = 1; player.y = LINES/2 - 1;  // player start location
+    player.x = 1; player.y = LINES - 2;  // player start location
+
+    struct obj_RAPA rapa;
+    rapa.x = max_x-rapa.shape_size_x-1; rapa.y = LINES - 2;
+
     int timeCounter = 0;    //time counter
 
 	while((ch != 'q') && (ch != 'Q')){
+
 		display_map(map, max_y, max_x);
         player.appear(player.y,player.x);
+        if(timeCounter>10){   //test generate rapa
+            rapa.appear(rapa.y, rapa.x);
+            rapa = moveObj(timeCounter, rapa);
+        }
         string timestring = "time: " + to_string(timeCounter);  //show time
         mvaddstr(1, 1, timestring.c_str()); 
         ch = getch();
@@ -110,4 +120,20 @@ struct Player gravityOfPlayer(int counter, struct Player player){
         if (is_move_ok(player.y + 1,player.x)){player.y = player.y + 1;}
     }
     return player;
+}
+
+struct obj_RAPA moveObj(int counter, struct obj_RAPA rapa){
+    int maxtime = 10;
+    if(counter%maxtime==maxtime-1){
+        rapa.x = rapa.x - 1;
+    }
+    return rapa;
+}
+
+struct obj_RAPA generateObjRAPA(int counter){
+    int maxtime = 6;
+    if(counter%maxtime==maxtime-1){
+        struct obj_RAPA rapa;
+        return rapa;
+    }
 }
