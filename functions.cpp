b0661dc09ea0f7baac4fa0a7b222e167ff2be987
Junhee Cho <junhee3085@gmail.com>
2020-11-19 11:08:52 +0900

    limit area to move player

--- functions.cpp
+++ functions.cpp
@@ -1,5 +1,6 @@
 #include <curses.h>
 #include <string>
+using namespace std;
 #define E_TRACE ' '
 
 struct Player
@@ -7,9 +8,20 @@ struct Player
     int x, y, life = 3; // current location and survival life
     static const int shape_size_y = 3, shape_size_x = 11;
     void appear(const int &move_y, const int &move_x){
-        mvaddstr(move_y-this->shape_size_y+1, move_x,  " --------- ");    //Tayo bus
+        mvaddstr(move_y-this->shape_size_y+1, move_x, " --------- ");    //Tayo bus
         mvaddstr(move_y-this->shape_size_y+2, move_x,"|      |OO|");
         mvaddstr(move_y-this->shape_size_y+3, move_x," --@--@--- ");
+    }
+};
+
+struct Obj_RAPA_python
+{
+    int x, y; // current location and survival life
+    static const int shape_size_y = 3, shape_size_x = 10;
+    void appear(const int &move_y, const int &move_x){
+        mvaddstr(move_y-this->shape_size_y+1, move_x, " -------- ");    //Tayo bus
+        mvaddstr(move_y-this->shape_size_y+2, move_x,"| Python |");
+        mvaddstr(move_y-this->shape_size_y+3, move_x," -------- ");
     }
 };
 
@@ -41,7 +53,8 @@ void game_start(){
 	while((ch != 'q') && (ch != 'Q')){
 		display_map(map, max_y, max_x);
         player.appear(player.y,player.x);
-        mvaddstr(1, 1,  "!!!!!!!!!!!"); 
+        string timestring = "time: " + to_string(timeCounter);  //show time
+        mvaddstr(1, 1, timestring.c_str()); 
         ch = getch();
         player = move2direction(ch, player);
         player = gravityOfPlayer(timeCounter, player);
@@ -55,7 +68,7 @@ void map_init(char **map, int max_y, int max_x){
     for(int i=0; i<max_y; i++){
         for (int j=0; j<max_x; j++){
             map[i][j] = ' ';
-            if(i==0 || i==max_y-1){
+            if(i==0 || i==max_y-1 || i==2){
                 map[i][j] = '*';
             }
         }
@@ -74,7 +87,7 @@ void display_map(char **map, int max_y, int max_x){
 int is_move_ok(int y, int x){   // 아래 충돌은 문제 없음 위 충돌 및 윈도우 밖으로 안나가도록 출동 판정 업그레이드 필요
     int compare_ch;
     compare_ch = mvinch(y,x); // 주어진 위치 문자 return
-    return !((compare_ch=='*'));
+    return !((compare_ch=='*' || y >LINES || y<3 || x > COLS- 70 || x < 0));
 }
 
 struct Player move2direction(int ch, struct Player player){
@@ -92,7 +105,7 @@ struct Player move2direction(int ch, struct Player player){
 }
 
 struct Player gravityOfPlayer(int counter, struct Player player){
-    int maxtime = 6;
+    int maxtime = 10;
     if(counter%maxtime==maxtime-1){
         if (is_move_ok(player.y + 1,player.x)){player.y = player.y + 1;}
     }
